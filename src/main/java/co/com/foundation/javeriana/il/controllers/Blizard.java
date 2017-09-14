e76b06028c914e6162cbd51dc2ab2ed9c860ac0a
albertofbs15 <AHernandezS@pslcol.com.co>
2017-09-14 13:52:12 -0500

    adding key to blizard file name

--- src/main/java/co/com/foundation/javeriana/il/controllers/Blizard.java
+++ src/main/java/co/com/foundation/javeriana/il/controllers/Blizard.java
@@ -20,60 +20,28 @@ public class Blizard implements Processor {
 
     private final String LEFT_PAD = "0";
     private final String DATE_FORMAT = "yyyyMMdd";
-    private final String key;
 
     public Blizard() {
         super();
-        key = setKey();
     }
 
-    public boolean hasEvent(Exchange exchange){
+    @Override
+    public void process(Exchange exchange) throws Exception {
         AircratlineMessage aircratlineMessage = exchange.getIn().getBody(AircratlineMessage.class);
-        return aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo() != null && aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping() != null;
-    }
-
-    public boolean hasEvent(AircratlineMessage aircratlineMessage){
-        return aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo() != null && aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping() != null;
-    }
-
-    public String transform(Exchange exchange) throws Exception {
-        AircratlineMessage aircratlineMessage = exchange.getIn().getBody(AircratlineMessage.class);
-
         DomainEventsInfo.OnAirShopping onAirShopping = aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping();
-
         StringBuilder line = new StringBuilder();
+        final String key = getKey();
         line.append(key)
                 .append(leftPad(onAirShopping.getCreditCardNumber(), 10))
                 .append(XMLGregorianCalendarToStringFormat(onAirShopping.getTransactionDate(), DATE_FORMAT))
                 .append(leftPad("" + onAirShopping.getValue(), 5))
                 .append(leftPad(onAirShopping.getDescription(), 67))
         ;
-        return line.toString();
-    }
-
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        AircratlineMessage aircratlineMessage = exchange.getIn().getBody(AircratlineMessage.class);
-
-        if (hasEvent(aircratlineMessage)) {
-            DomainEventsInfo.OnAirShopping onAirShopping = aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping();
-
-            StringBuilder line = new StringBuilder();
-            line.append(key)
-                    .append(leftPad(onAirShopping.getCreditCardNumber(), 10))
-                    .append(XMLGregorianCalendarToStringFormat(onAirShopping.getTransactionDate(), DATE_FORMAT))
-                    .append(leftPad("" + onAirShopping.getValue(), 5))
-                    .append(leftPad(onAirShopping.getDescription(), 67))
-            ;
-            exchange.getIn().setBody(line.toString());
-        }
+        exchange.getIn().setBody(line.toString());
+        exchange.getIn().setHeader("key", key);
     }
 
     public String getKey() {
-        return key;
-    }
-
-    private String setKey() {
         return UUID.randomUUID().toString().substring(0, 10);
     }
 
