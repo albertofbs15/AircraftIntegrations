160a4b948db64b6af84eec784e4dfb69843109a0
albertofbs15 <AHernandezS@pslcol.com.co>
2017-09-14 13:18:53 -0500

    Release 1.0 ... Blizard funcionando

--- src/main/java/co/com/foundation/javeriana/il/controllers/Blizard.java
+++ src/main/java/co/com/foundation/javeriana/il/controllers/Blizard.java
@@ -1,8 +1,9 @@
 package co.com.foundation.javeriana.il.controllers;
 
+import co.com.foundation.javeriana.il.model.AircratlineMessage;
 import co.com.foundation.javeriana.il.model.DomainEventsInfo;
-import co.com.foundation.javeriana.il.model.FlightLeg;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,47 +14,71 @@ import java.util.UUID;
 import static util.UtilDate.XMLGregorianCalendarToStringFormat;
 
 @Component
-public class Blizard {
+public class Blizard implements Processor {
+
+    Logger LOGGER = LoggerFactory.getLogger(Blizard.class);
 
     private final String LEFT_PAD = "0";
     private final String DATE_FORMAT = "yyyyMMdd";
     private final String key;
 
-	public Blizard() {
-		super();
-		key = setKey();
-	}
+    public Blizard() {
+        super();
+        key = setKey();
+    }
 
-	public void transform(final Exchange exchange, String body) {
-        Logger LOGGER = LoggerFactory.getLogger(Blizard.class);
-	    LOGGER.info("[AHS LOG] ------------" + body);
-        LOGGER.info("[AHS LOG] ------------" + exchange.getIn().getBody());
+    public boolean hasEvent(Exchange exchange){
+        AircratlineMessage aircratlineMessage = exchange.getIn().getBody(AircratlineMessage.class);
+        return aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo() != null && aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping() != null;
+    }
 
-		FlightLeg flightLeg = exchange.getIn().getBody(FlightLeg.class);
-        DomainEventsInfo x = flightLeg.getDomainEventsInfo();
-        DomainEventsInfo.OnAirShopping onAirShopping = x.getOnAirShopping();
-		StringBuilder line = new StringBuilder();
-        line    .append(key)
-				.append(leftPad(onAirShopping.getCreditCardNumber() ,10))
+    public boolean hasEvent(AircratlineMessage aircratlineMessage){
+        return aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo() != null && aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping() != null;
+    }
+
+    public String transform(Exchange exchange) throws Exception {
+        AircratlineMessage aircratlineMessage = exchange.getIn().getBody(AircratlineMessage.class);
+
+        DomainEventsInfo.OnAirShopping onAirShopping = aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping();
+
+        StringBuilder line = new StringBuilder();
+        line.append(key)
+                .append(leftPad(onAirShopping.getCreditCardNumber(), 10))
                 .append(XMLGregorianCalendarToStringFormat(onAirShopping.getTransactionDate(), DATE_FORMAT))
-                .append(leftPad("" + onAirShopping.getValue() ,5))
-                .append(leftPad(onAirShopping.getDescription() ,67))
-            ;
-		exchange.getIn().setBody( line.toString() );
+                .append(leftPad("" + onAirShopping.getValue(), 5))
+                .append(leftPad(onAirShopping.getDescription(), 67))
+        ;
+        return line.toString();
+    }
 
-        exchange.getIn().setBody("alberto");
-	}
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        AircratlineMessage aircratlineMessage = exchange.getIn().getBody(AircratlineMessage.class);
+
+        if (hasEvent(aircratlineMessage)) {
+            DomainEventsInfo.OnAirShopping onAirShopping = aircratlineMessage.getFlightLeg().get(0).getDomainEventsInfo().getOnAirShopping();
+
+            StringBuilder line = new StringBuilder();
+            line.append(key)
+                    .append(leftPad(onAirShopping.getCreditCardNumber(), 10))
+                    .append(XMLGregorianCalendarToStringFormat(onAirShopping.getTransactionDate(), DATE_FORMAT))
+                    .append(leftPad("" + onAirShopping.getValue(), 5))
+                    .append(leftPad(onAirShopping.getDescription(), 67))
+            ;
+            exchange.getIn().setBody(line.toString());
+        }
+    }
 
     public String getKey() {
         return key;
     }
 
     private String setKey() {
-        return UUID.randomUUID().toString().substring(0,10);
+        return UUID.randomUUID().toString().substring(0, 10);
     }
 
     private String leftPad(String field, int lenght) {
         return StringUtils.leftPad("129018", 10, LEFT_PAD);
     }
-	
+
 }
